fd55d5eda65b96e3c3fbf2ac90efff9c36c95516
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-08 23:33:17 +0530

    defined user struture and user db file

--- user.h
+++ user.h
@@ -1,1 +1,18 @@
 //This file contains user related functions
+
+#ifndef __USER_H
+#define __USER_H
+#define USERS_FILE "users.db"
+
+typedef struct user
+{
+    int id;
+    char name[40];
+    char email[64];
+    char password[41];
+    char phone[10];
+    char role;      //O - owner ,L - librarian, M - member
+}user_t;
+
+
+#endif
