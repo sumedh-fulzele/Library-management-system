9e80c1dd1591998ec429d6329a1f2d758b5bab3c
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-08 23:40:58 +0530

    added user_save function

--- user_dal.c
+++ user_dal.c
@@ -1,1 +1,26 @@
 //This files contains user related data handling functions(Tier - 1).
+
+#include<stdio.h>
+#include<string.h>
+#include"user.h"
+
+int user_save(user_t *u){
+    FILE *fu;
+    
+    fu = fopen(USERS_FILE, "ab");
+    if(fu == NULL){
+        return 1;
+    }
+    
+    size_t recordwritten = fwrite(u, sizeof(user_t), 1,fu);
+    fclose(fu);
+    
+    if(recordwritten != 1)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
--- user_dal.h
+++ user_dal.h
@@ -1,1 +1,13 @@
 //This files contains user related data handling functions(Tier - 1).
+
+#ifndef __USER_DAL_H
+#define __USER_DAL_H
+
+#include"user.h"
+
+int user_save(user_t *u);
+int user_update(user_t *u);
+int user_find_by_email(char* , user_t *u);
+int user_find_by_id(user_t *u);
+
+#endif
