91ab90c4f2900d66a43d46d36f02ac6234fa02de
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-09 13:08:57 +0530

    added header files

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
+#include"user.h"
+#include"user_dal.h"
+#include"user_service.h"
+#include"user_control_dummy.h"
+#include<string.h>
+
 
 int main()
-{
-    
-    
+{   
     return 0;
 }
