cbf16e9d50095b8ae486c0bb045a643a394a799b
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-10 19:53:44 +0530

    changed  user_t size

--- user.h
+++ user.h
@@ -11,10 +11,10 @@ enum rol {owner, librarian, member};
 typedef struct user
 {
     int id;
-    char name[42];
+    char name[41];
     char email[60];
     char password[40];
-    char phone[10];
+    char phone[11];
     enum rol role;      //O - owner ,L - librarian, M - member
 }user_t;
 
