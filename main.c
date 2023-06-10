11d69cc372e9bc7ab9693da60d3020e6b092ff20
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-11 01:48:03 +0530

    some minor changes in main.c

--- main.c
+++ main.c
@@ -2,19 +2,21 @@
 #include<stdlib.h>
 #include<string.h>
 #include"user.h"
-#include"user_dal.h"
-#include"user_service.h"
 #include"user_control.h"
+#include"user_service.h"
 
 
 int main(int argc, char *argv[]) {
     if(argc == 2 && strcmp(argv[1], "--init") == 0) {
         user_t owner;
-        accept_user(&owner);
+        user_accept(&owner);
         
         if(owner_register(&owner) == 1){
             printf("Intialization is successful.\n");
         }
         exit(0);
     }
+
+
+    return 0;
 }
