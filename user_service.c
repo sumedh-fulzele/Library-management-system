78e2e5897c796f4bb997ce8f17e9c73f40b4462c
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-10 23:01:38 +0530

    added user and librarian registration function

--- user_service.c
+++ user_service.c
@@ -2,14 +2,13 @@
 
 #include<stdio.h>
 #include<string.h>
-#include"user.h"
 #include"user_dal.h"
 #include"user_service.h"
-#include"user_control.h"
 
 //validate user and return respective user data in *u.
 int user_authenticate(char *email, char *password, user_t *u){
     int found = 0;
+        printf("email found");
     //comapares given email and password in db.
     if(user_find_by_email(email, u) == 1){
         if(strcmp(u->password,password) == 0){
@@ -33,7 +32,7 @@ int owner_register(user_t *u){
     u->role = owner;
     
     remove(USERS_FILE);
-    if(user_save(&u) == 1){
+    if(user_save(u) == 1){
         return 1;
     }
     else{
@@ -41,3 +40,28 @@ int owner_register(user_t *u){
     }
 
 }
+
+int member_register(user_t *u){
+    u->id = get_max_id() + 1;
+    u->role = member;
+
+    if(user_save(u) == 1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    }
+
+ int librarian_register(user_t *u){
+    u->id = get_max_id() + 1;
+    u->role = librarian;
+
+    if(user_save(u) == 1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+ }
+
--- user_service.h
+++ user_service.h
@@ -7,5 +7,8 @@
 
 int user_authenticate(char *email, char *password , user_t *u);  //validate user and return respective user data in u.
 int owner_register(user_t *u);
+int member_register(user_t *u);
+int librarian_register(user_t *u);
+
 
 #endif
