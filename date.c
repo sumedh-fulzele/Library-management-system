07e8c5fabadb0d614b6b481a5ca224c3b9a7234d
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-16 12:20:27 +0530

    added functions to handle user inputed dates

--- date.c
+++ date.c
@@ -2,16 +2,28 @@
 #include <time.h>
 #include"date.h"
 
+struct tm d1;
+struct tm d2; 
+date_t date1, date2;
 //date should be adjusted like below.
 // year - 1900
 // month (0 - 11)
 // day of the month (1 - 31)
+void convert(date_t *date, struct tm *d){
+    d->tm_year = date->year -1900;
+    d->tm_mon = date->month - 1;
+    d->tm_mday = date->day;
+}
 
 
 int date_diff(date_t date1, date_t date2 ){
     
-    time_t time1 = mktime(&date1);
-    time_t time2 = mktime(&date2);
+    struct tm d1, d2;
+    convert(&date1, &d1);
+    convert(&date2, &d2);
+
+    time_t time1 = mktime(&d1);
+    time_t time2 = mktime(&d2);
 
     double diff_in_seconds = difftime(time1, time2);
     int diff_in_days = (int)(diff_in_seconds / (24 * 3600));
@@ -19,24 +31,31 @@ int date_diff(date_t date1, date_t date2 ){
     return diff_in_days;
 }
 
-void adddays(date_t i_date, int days_to_add,date_t *d_date ) {
-    i_date.tm_mday += days_to_add;
+void adddays(date_t i_date, int days_to_add, date_t *d_date ) {
+    
+    struct tm d1;
+    convert(&i_date, &d1);
+    // convert(&d_date, &d2);
+    
+    d1.tm_mday += days_to_add;
 
-    while (i_date.tm_mday > days_in_month(i_date.tm_mon, i_date.tm_year)) {
-        i_date.tm_mday -= days_in_month(i_date.tm_mon, i_date.tm_year);
-        i_date.tm_mon++;
+    while (d1.tm_mday > days_in_month(d1.tm_mon, d1.tm_year)) {
+        d1.tm_mday -= days_in_month(d1.tm_mon, d1.tm_year);
+        d1.tm_mon++;
 
-        if (i_date.tm_mon > 11) {
-            i_date.tm_mon = 0;
-            i_date.tm_year++;
+        if (d1.tm_mon > 11) {
+            d1.tm_mon = 0;
+            d1.tm_year++;
         }
     }
-    d_date->tm_year = i_date.tm_year;
-    d_date->tm_mon  = i_date.tm_mon;
-    d_date->tm_mday = i_date.tm_mday;
+    d_date->year = d1.tm_year + 1900;
+    d_date->month = d1.tm_mon + 1;
+    d_date->day = d1.tm_mday;
+
 }
 
 int is_leap_year(int year) {
+    year += 1900;
     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
         return 1;
     return 0;
@@ -50,4 +69,11 @@ int days_in_month(int month, int year) {
         days++;
 
     return days;
+}
+
+void curren_date(date_t *date){
+    time_t current_time;
+    
+    current_time = time(NULL);
+    date = localtime(&current_time);
 }
--- date.h
+++ date.h
@@ -3,7 +3,12 @@
 
 #include<time.h>
 
-typedef struct tm date_t; 
+typedef struct date
+{
+    int day;
+    int month;
+    int year;
+}date_t;
 
 int date_diff(date_t date1, date_t date2);  //return difference between two  dates in number of days
 void adddays(date_t i_date, int days_to_add,date_t *d_date );
