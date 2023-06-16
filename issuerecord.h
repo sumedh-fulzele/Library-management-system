fc51677f7b92098f3316c50f59b0dbd3eab2f0bb
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-16 09:03:55 +0530

    created issuerecord sturcture

---
+++ issuerecord.h
@@ -0,0 +1,20 @@
+#ifndef __ISSUERECORD_H
+#define __ISSUERECORD_H
+
+#include"date.h"
+#include<time.h>
+
+typedef struct issuerecord
+{
+    int id;
+    int book_copy_id;
+    int member_id;
+    int fine_amount;
+    date_t issue_date;
+    date_t return_due_date;
+    date_t return_date;
+
+}issuerecord_t;
+
+
+#endif
