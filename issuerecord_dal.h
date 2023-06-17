432637e26daa35a886df836cae7033ff3c0a7a35
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-17 22:49:37 +0530

    added etries in header file

--- issuerecord_dal.h
+++ issuerecord_dal.h
@@ -6,5 +6,6 @@
 int issuerecord_save(issuerecord_t *ir);
 int get_max_issuerecord_id();
 int issuerecord_update(issuerecord_t *ir);
+int issuerecord_find_by_id(int issuerecord_id, issuerecord_t *ir);
 
 #endif
