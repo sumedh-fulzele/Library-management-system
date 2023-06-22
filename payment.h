ea82c9393f846934002f55676893f56e2e0320e6
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-22 11:39:49 +0530

    defined payment struct and some macros

---
+++ payment.h
@@ -0,0 +1,21 @@
+#ifndef __PAYMENT_H
+#define __PAYMENT_H
+
+#include<stdio.h>
+#include"date.h"
+
+#define PAYMENT_FILE    "payment.db"
+#define RECSIZE_PAYMENT sizeof(payment_t)
+
+enum payment_type {fine, fees};
+
+typedef struct payment{
+    int id;
+    int member_id;
+    float amount;
+    enum payment_type pt;
+    date_t transaction_time;
+    date_t nextpayment_duedate;
+}payment_t;
+
+#endif
