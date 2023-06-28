c32d6a767804162112bb9cccb17b07c8ecd0cdfa
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-28 09:35:38 +0530

    added find_by_isbn function

--- book_control.c
+++ book_control.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include"book.h"
-#include"book_service.h"
+#include <stdio.h>
+#include "book.h"
+#include "book_service.h"
 
 void book_accept(book_t *b){
     printf("\nEnter the book details.");
@@ -15,6 +15,7 @@ void book_accept(book_t *b){
     scanf("%*c");
     printf("Price : ");
     scanf("%f", &b->price);
+    
 }
 
 void book_print(book_t *b){
@@ -22,13 +23,16 @@ void book_print(book_t *b){
     printf("\nAuthor : %s", b->author);
     printf("\nISBN : %s", b->isbn);
     printf("\nCategory : %s", b->category);
-    printf("\nPrice : %f", b->price);
+    printf("\nPrice : %f\n", b->price);
 }
 
 void add_new_book(){
     book_t b;
+    int rack;
     book_accept(&b);
-    int flag_add = book_add(&b);
+    printf("Enter the rack no. : ");
+    scanf("%d", &rack);
+    int flag_add = book_add(&b) && book_copy_add(b.isbn, rack);
     if( flag_add == 1){
         printf("Book is successfully added..!!");
     }
@@ -37,32 +41,35 @@ void add_new_book(){
     }
 }
 
-// void find_book(){
-//     book_t  b;
-//     char isbn[BOOK_ISBN_SIZE];
-//     printf("Enter the ISBN of the book which you wish to search : ");
-//     scanf("%[^\n]",isbn);
-//     if(book_search_by_isbn(isbn, &b) == 1){
-//         printf("\nBook Found..!!\n");
-//         book_print(&b);
-//     }
-//     else{
-//         printf("\nBook Not Found..!!");
-//     }
-// }
-
-void find_book_by_title(){
+void find_book_by_isbn(){
     book_t  b;
-    char title[BOOK_TITLE_SIZE];
-    printf("Enter the title of the book : ");
-    scanf("%[^\n]",title);
-    if(book_search_by_title(title) == 1){
+    char isbn[BOOK_ISBN_SIZE];
+    printf("\nEnter the ISBN of the book which you wish to search : ");
+    scanf("%[^\n]",isbn);
+    getchar();
+    if(book_search_by_isbn(isbn, &b) == 1){
         printf("\nBook Found..!!\n");
         book_print(&b);
     }
     else{
         printf("\nBook Not Found..!!");
     }
+}
+
+void find_book_by_title(){
+
+    char title[BOOK_TITLE_SIZE];
+   
+    printf("Enter the title of the book : ");
+    scanf("%[^\n]",title);
+   
+    if(book_search_by_title(title) == 1){
+        printf("\nSearch Results\n");
+    }
+    else{
+        printf("\nBook not found..!!\n");
+    }
+
 }
 
 void edit_book(){
@@ -76,11 +83,6 @@ void edit_book(){
         printf("Failed to edit book..!!");
     }
 }
-
-// void check_book_availability(){
-    
-
-// }
 
 void book_copy_accept(char isbn[BOOK_ISBN_SIZE], int *rack){
     printf("\nEnter Book Copy details..");
--- book_control.h
+++ book_control.h
@@ -1,12 +1,16 @@
 #ifndef __BOOK_CONTROL_H
 #define __BOOK_CONTROL_H
 
+#include "book.h"
+
 void book_accept(book_t *b);
 void book_print(book_t *b);
 void add_new_book();
-void find_book_by_title();
+void find_book_by_isbn();
 void edit_book();
 void add_new_book_copy();
 void change_rack();
 void check_book_availability();
+void find_book_by_title();
+
 #endif
