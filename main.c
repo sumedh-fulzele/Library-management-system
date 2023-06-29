2afb1d5a71c10099db8a3cdcb529d5953100ab24
Sumedh Fulzele <sumedh7478@gmail.com>
2023-06-29 15:24:42 +0530

    final main.c file

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"user.h"
-#include"user_control.h"
-#include"user_service.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "user.h"
+#include "mainmenu.h"
 
 int main(int argc, char *argv[]) {
+    
     if(argc == 2 && strcmp(argv[1], "--init") == 0) {
         user_t owner;
         user_accept(&owner);
-        
+
         if(owner_register(&owner) == 1){
             printf("Intialization is successful.\n");
         }
+        
         exit(0);
     }
-
-
+    int user_id;
+    enum rol user_role;
+    loginpage_switch(&user_id, &user_role);
+    
     return 0;
 }
